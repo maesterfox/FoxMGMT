107a9ceb2ebaa02a84b85dc2970bf04366f386b4
maesterfox <talentedmrfox@hotmail.com>
2024-04-30 12:09:48 +0100

    App.js Apollo update to use import { BrowserRouter as Router, Route, Routes } from "react-router-dom";

--- client/src/App.js
+++ client/src/App.js
@@ -25,7 +25,8 @@ const cache = new InMemoryCache({
 });
 
 const client = new ApolloClient({
-  uri: process.env.REACT_APP_GRAPHQL_API || "http://localhost:5000/graphql",
+  uri:
+    "https://foxmgmt.onrender.com/graphql" || "http://localhost:5000/graphql",
   cache: cache,
 });
 
