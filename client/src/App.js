92a5c54cbd215b16d0a7fbcbdd5daab92bf07f3d
maesterfox <talentedmrfox@hotmail.com>
2024-04-30 10:53:07 +0100

    index.js: changing routes from build to static

--- client/src/App.js
+++ client/src/App.js
@@ -25,8 +25,8 @@ const cache = new InMemoryCache({
 });
 
 const client = new ApolloClient({
-  uri: "http://localhost:5000/graphql",
-  cache,
+  uri: process.env.REACT_APP_GRAPHQL_API || "http://localhost:5000/graphql",
+  cache: cache,
 });
 
 function App() {
