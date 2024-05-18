38246788bbe3d479b97b3ca3043b8430ca684a08
maesterfox <talentedmrfox@hotmail.com>
2024-05-18 16:10:47 +0100

    Update CORS configuration

--- api/index.js
+++ api/index.js
@@ -1,11 +1,30 @@
 const path = require("path");
 const express = require("express");
+const cors = require("cors");
 const graphql = require("./graphql");
 const app = express();
 
+// Configure CORS
+const allowedOrigins = [
+  "https://fox-mgmt-maesterfoxs-projects.vercel.app",
+  "https://foxmgmt.davidfoxdev.co.uk",
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.static(path.join(__dirname, "../client/build")));
 
-app.use("/graphql", graphql);
+app.use("/api/graphql", graphql);
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
